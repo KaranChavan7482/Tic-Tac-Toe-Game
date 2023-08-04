439da3c292b7bcfd722f08d25be4aa7861770d15
KaranC <karanc2310@gmail.com>
2023-08-04 22:52:14 +0530

    Added Background Circles

--- src/App.jsx
+++ src/App.jsx
@@ -94,6 +94,8 @@ function App() {
         Current Game History
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+
+      <div className="bg-balls" />
     </div>
   );
 }
